ee63f1743bd8589bb367c8146d8c6620634c27f7
viidit678 <viditthakuri944@gmail.com>
2025-06-03 23:19:40 +0545

    Update 03operator.js with latest changes

---
+++ 03operator.js
@@ -0,0 +1,14 @@
+// D =(L-S)/L * 100;
+ 
+// a+((b*c)/d)-e+f;
+
+var sellingPrice = 199;
+var listingPrice  =799;
+var discountPercentage =(listingPrice-sellingPrice)/listingPrice *100;
+console.log("Discount percentage is :" + discountPercentage);
+
+displaydiscountPercentage = (listingPrice-sellingPrice)/listingPrice *100;
+console.log(displaydiscountPercentage + " % off ");
+ var result =listingPrice > sellingPrice;
+ console.log(result);
+ console.log(typeof result);// typeof =type of data holds
