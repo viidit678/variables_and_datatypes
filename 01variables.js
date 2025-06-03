0f47732017c8c51babe4ea8d719aeb947c4ca66c
viidit678 <viditthakuri944@gmail.com>
2025-06-03 16:53:58 +0545

    02code_of_js

---
+++ 01variables.js
@@ -0,0 +1,23 @@
+var fullName="viidit thakuri";
+var age =21;
+var isStudent = true;
+// how to name a variables
+// fullName  CAMELCASE
+// full-Name KEBABCASE
+// full_name SNAKECASE
+// FullName
+
+var courseName = "javascript";
+
+var isLoggedIN= false;
+
+var LoggedCount = 1000;
+// console.log(LoggedCount);
+// console.log("LoggedCount");
+
+
+var paymentMode;
+console.log(paymentMode);
+var paymentMode = "credit card";
+console.log(paymentMode);// credit card
+ 
